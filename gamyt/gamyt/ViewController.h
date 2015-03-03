902d1cf0cd07f4ff3191301d7cd770fecef0b089
yilongchun <5115413@qq.com>
2015-03-03 15:11:22 +0000

    登录界面完成

--- gamyt/gamyt/ViewController.h
+++ gamyt/gamyt/ViewController.h
@@ -8,8 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITextFieldDelegate>
 
+@property (weak, nonatomic) IBOutlet UITextField *account;
+@property (weak, nonatomic) IBOutlet UITextField *password;
 
 @end
 
--- gamyt/gamyt/ViewController.m
+++ gamyt/gamyt/ViewController.m
@@ -17,8 +17,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIView *leftview = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 30, 50)];
+    UIImageView *usernameImg = [[UIImageView alloc] initWithFrame:CGRectMake(5, 15, 20, 20)];
+    [usernameImg setImage:[UIImage imageNamed:@"login_username_icon.png"]];
+    [leftview addSubview:usernameImg];
+    self.account.leftViewMode = UITextFieldViewModeAlways;
+    self.account.leftView = leftview;
+    
+    UIView *leftview2 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 30, 50)];
+    UIImageView *passwordImg = [[UIImageView alloc] initWithFrame:CGRectMake(5, 15, 20, 20)];
+    [passwordImg setImage:[UIImage imageNamed:@"login_password_icon.png"]];
+    [leftview2 addSubview:passwordImg];
+    self.password.leftViewMode = UITextFieldViewModeAlways;
+    self.password.leftView = leftview2;
+    
+    [self.account setValue:[UIColor whiteColor] forKeyPath:@"_placeholderLabel.textColor"];
+    [self.password setValue:[UIColor whiteColor] forKeyPath:@"_placeholderLabel.textColor"];
+    
+    [self.account setTintColor:[UIColor whiteColor]];
+    [self.password setTintColor:[UIColor whiteColor]];
+//    [[UITextField appearance] setTintColor:[UIColor whiteColor]];
 }
 
+- (void)textFieldDidBeginEditing:(UITextField *)textField{
+    
+    [textField setBackgroundColor:[UIColor colorWithRed:26/255.0 green:73/255.0 blue:130/255.0 alpha:1.0]];
+    
+    NSLog(@"textFieldDidBeginEditing");
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField{
+    [textField setBackgroundColor:[UIColor colorWithRed:36/255.0 green:102/255.0 blue:171/255.0 alpha:1.0]];
+    NSLog(@"textFieldDidEndEditing");
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
